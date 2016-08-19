759fde750d9a654a11150c85db1dd8b4e96a51c8
Mitch Masterson <mitchell.masterson@gmail.com>
2016-08-19 01:17:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0, l = lis.length; i < l; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelectorAll('div#grand-node div')
+
+  return node[node.length - 1]
+}
