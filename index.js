0484e227eaec635f76c1c1972924673955532db5
Cindy Gao <cindygao@hotmail.com>
2016-08-29 13:35:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//accepts a selector and returns the
+//first element that matches
+function getFirstSelector(selector){
+  var idList;
+  if (document.getElementById(selector)!= null){
+    idList = document.getElementById(selector)
+  }
+  else if (document.getElementByClassName(selector)!= null){
+    idList = document.getElementByClassName(selector)
+  }
+  else if (document.getElementByTagName(selector)!= null){
+    idList = document.getElementByTagName(selector)
+  }
+  else{
+    idList = []
+  }
+  return idList[0]
+}
+
+//pulls a .target out of #nested (# is
+//used for IDs in selectors
+
+//Note that in index.html #nested and .target
+//just happen to be divs. This method should
+//work with arbitrary elements.)
+function nestedTarget(){
+
+}
+
+//increases the ranks in all of the
+//.ranked-lists by n
+function increaseRankBy(n){
+
+}
+//pulls out the most deeply nested child
+//from div#grand-node
+function deepestChild(){
+
+}
