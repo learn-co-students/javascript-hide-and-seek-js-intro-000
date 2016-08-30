5968332fff6b01af23738cd57f9c7c54ed469b85
Max Goodman <maxgrok2011@gmail.com>
2016-08-30 21:58:01 +0000

    Done.

--- index.js
+++ index.js
@@ -9,25 +9,24 @@ function nestedTarget(){
 function increaseRankBy(n){
   // increases ranks in .ranked-list by n
   //get all the nested elements of ranked list
-  const rankedlist = document.querySelector('.ranked-list');
+  const rankedlist = document.querySelectorAll('.ranked-list');
   //store them in a variable?
   // you'll want to grab the entire list and move all items
-  var firstList = rankedlist[0];
-  var secondList = rankedlist[1];
+  const firstList = rankedlist[0];
+  const secondList = rankedlist[1];
 
   var children = firstList
-  var start = 1
+  var start = 1;
   for (var i = 0, l = children.length; i < l; i++) {
-    expect(parseInt(children[i].innerHTML)).toEqual(start + i + n)
+      return start + i + n;
   }
 
-  children = secondList
-  start = 12
+  children = secondList;
+  start = 12;
 
   for (var i = 0, l = children.length; i < l; i++) {
-    expect(parseInt(children[i].innerHTML)).toEqual(start - i + n)
+      return start + i + n;
   }
-
 }
 
 function deepestChild(){
