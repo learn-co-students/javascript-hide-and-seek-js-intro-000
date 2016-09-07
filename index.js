90eeb6529a89c2b34d18ef5fbe3c41fd81195c5a
Stephen Higgins <stephenhiggins76@gmail.com>
2016-09-07 19:26:54 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function getFirstSelector(selector) {
 }
 
 
+
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
@@ -15,4 +16,26 @@ function increaseRankBy(n) {
     itemInt += n
       rankList[i].innerHTML = itemInt.toString()
   }
+}
+
+function deepestChild() {
+var answer
+var children = {}
+var counter = 0
+var list = document.querySelectorAll('#grand-node > div')
+  for (var i = 0; i < list.length; i++) {
+    var current_child = list[i]
+    while (current_child.querySelector(':first-child') != null) {
+      counter += 1
+      current_child = current_child.querySelector(':first-child')
+      deepestChild = current_child
+
+}
+        if (children[i] === undefined || children[i] < counter) {
+          children[i] = counter
+          answer = deepestChild
+        }
+        counter = 0
+        }
+return answer
 }
