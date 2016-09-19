13d5e8edeb09f3249b7349f5f578a0552b3c5806
William Gough <william.a.gough@gmail.com>
2016-09-19 17:56:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  var num = 0
+  var list = document.querySelectorAll('.ranked-list li')
+  for(var i = 0; i<list.length; i++){
+    num = (parseInt(list[i].innerHTML)) + n;
+    (list[i].innerHTML) = num
+  }
+  return list
+}
