706c9c67d113f9284979d295417759477a25756f
Rent Sidon <rsidon@icloud.com>
2016-10-18 01:28:16 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
 var lists = document.querySelectorAll('.ranked-list');  //first find all nodes that share this class
   for (let i = 0; i < lists.length; i++) {
-       var ranks = lists[i]              //these are nodes within each list
+       var ranks = lists[i].children             //these are nodes within each list
      for (let j = 0; j < ranks.length; j++) {  // ranks[j] are the elements that get increased by n -
         ranks[j].innerHTML = (parseInt(ranks[j].innerHTML)) + n;  // change string value to integer and add n
 }
