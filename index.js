07e628f4bd2da66f1dd578ef26f3d1f9a3b1bdfc
Ronnie Smith <halfpint1170@gmail.com>
2016-12-25 07:07:43 +0000

    Done.

--- index.js
+++ index.js
@@ -8,20 +8,19 @@
 
  function deepestChild() {
    var element = document.querySelector('#grand-node');
-   while(true) {
-     var nextElement = element.querySelector('div');
-     if(nextElement == null) {
-       return element;
-     }
-     element = nextElement;
+   var next = element.children[0]
+   
+   while(next) {
+     element = next;
+     next = element.children[0]
    }
+   
+   return element;
  }
 
  function increaseRankBy(n) {
    var elements = document.querySelectorAll('.ranked-list');
    for(var i = 0; i < elements.length; i++) {
-     var rank = parseInt(elements[i].innerHTML);
-     rank++;
-     elements[i].innerHTML = rank;
+     elements[i].innerHTML = (parseInt(elements[i].innerHTML, 10) + n);
    }
  }
