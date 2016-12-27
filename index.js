3e74e1cd039ca869b7c978a8add47cee2e9f43f2
Vanessa Jimenez <vjimenez127@gmail.com>
2016-12-27 05:42:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node'.querySelector('div div div div')
+
+while ()
+
+
+}
