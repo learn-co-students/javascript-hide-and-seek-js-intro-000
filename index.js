21582c050437b8229752f092bd4e68a283b1fd16
Usman Pervaiz <de.usman@yahoo.com>
2017-01-23 06:04:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function getFirstSelector(selector) {
+
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+//return document.getElementById('nested').getElementsByClassName('target')[0];
+return document.querySelector("#nested div.target");
+}
+
+function increaseRankBy(n) {
+  /*var a = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  
+  for(var i = 0; i < a.length; i++) {
+    var b = parseInt(a[i].innerHTML);
+    a[i].innerHTML = (b + n).toString();
+  }*/
+  var a = document.getElementById('app');
+  var b = a.getElementsByClassName('ranked-list');
+  
+  for(var i = 0; i < b.length; i++) {
+      var x = parseInt(b[i].innerHTML);
+      b[i].innerHTML = (x + n).toString();
+  }
+  return;
+}
