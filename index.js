c61dcf59fcf91f6c9eb6c90bccdea345f7353a9c
Sylvia Lee <helloimsylvia@gmail.com>
2017-02-02 04:41:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  for (let i = 0, s < n.length; i < s; i++) {
+    return document.querySelector('.ranked-list')
+    
+  }
+}
