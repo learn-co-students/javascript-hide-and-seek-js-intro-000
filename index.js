89160a53e361ecea65f4f65f35e75556a9fc4006
Pamela Lee <pamela.jean.lee@gmail.com>
2017-05-08 15:40:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('.ranked-list');
+
+  for(let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+  return list;
+}
