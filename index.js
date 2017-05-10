bd87ef10774c293778635a094764d1fa052bac74
Thomas Koshy <ttkoshy@gmail.com>
2017-05-10 01:02:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selelctor){
+  document.querySelector(selector)
+  }
+
+function nestedTarget(){
+  document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+var list = document.querySelectorAll('ul.ranked-list li')
+for (let i = 0; i < list.length;i++{
+  list[i].innerHTML= parseInt(list[i].innerHTML, 10) + n;
+  }
+}
+
+function deepestChild() {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i=0, 1=list.length; i<1; i++) {
+    list[i].innerHTML = (i+1).toString()
+  }
