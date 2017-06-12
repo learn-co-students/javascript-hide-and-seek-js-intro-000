c8c2e6076562dacf33dbe1750410b8a522c5ec06
Daniel Alviso <dan.alviso2@yahoo.com>
2017-06-12 04:05:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('.ranked-list li')
+
+  ranks.forEach((rank) => {
+    rank.innerHTML = parseInt(rank.innerHTML) + n
+  })
+}
+
+function deepestChild() {
+  let deepestChild = document.querySelector('div#grand-node')
+
+  while (deepestChild.childElementCount > 0) {
+    deepestChild = deepestChild.children[0]
+  }
+
+  return deepestChild
+}
