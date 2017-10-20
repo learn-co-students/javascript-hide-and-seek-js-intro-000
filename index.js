773e75e8c8264d577514d3ba0a358598858ce11a
Kévin R <kevin.rabesaotra@gmail.com>
2017-10-20 19:34:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  var table = document.querySelectorAll('.ranked-list');
+  debugger;
+  for(var i = 0; i < table.length; i++){
+
+  }
+}
