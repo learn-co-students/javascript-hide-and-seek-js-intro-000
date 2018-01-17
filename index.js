66e94d56b6b829d9180abd48d4973fc45dd6a1d5
Abhijit <abhijit.khanna@live.com>
2018-01-17 23:21:47 +0000

    Done.

--- index.js
+++ index.js
@@ -6,5 +6,30 @@ function getFirstSelector(selector) {
 
 function nestedTarget() {
 
-  return document.querySelector('#nested.target')
+  //return document.getElementById('nested').getElementsByClassName('target')
+  return document.querySelector('#nested .target')
+
+}
+
+function increaseRankBy(n) {
+
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list li')
+
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+
+}
+
+function deepestChild() {
+
+  let current = document.getElementById('grand-node')
+  var next
+
+  next = current.querySelectorAll('div')
+  while(next[0]) {
+    current = next[0]
+    next = current.querySelectorAll('div')
+  }
+  return current
 }
