4cee49ab63c97c0e7a443758e00a82994e900fc6
Richard Melchior <raustin.melchior@gmail.com>
2018-05-24 19:00:47 +0000

    Done.

--- index.js
+++ index.js
@@ -23,18 +23,17 @@ function deepestChild(){
     }
   }
 /**
-  return lis[lis.length-1] // This is how I first tried to do it.
+  return lis[lis.length-1] // This is how I first tried to do it. This works.
 
 
   function deepestChild() {
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
+    let node = document.getElementById('grand-node')
+    let nextNode = node.children[0]
 
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
-  }
-
+    while (nextNode) {
+      node = nextNode
+      nextNode = node.children[0]
+    }
     return node
   } //This is how the program wanted it done
 
