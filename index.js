a14046955245d907892207ec83f5a53438d29c2b
Martin Cervantes <cervantes.martine@gmail.com>
2018-07-06 23:53:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  var rank;
+  var track = [];
+  const nodelist = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < nodelist.length; i++) {
+    rank = parseInt(nodelist[i].innerHTML)
+    nodelist[i].innerHTML = (rank + n).toString();
+  }
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelectorAll('div')[3]
+}
