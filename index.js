8fb3571a9f3c0a67b4bb510603220bfeeece0e7b
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-07-10 18:59:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget (nested, target) {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy (n) {
+  const list = document.querySelectorAll('.ranked-list')
+  for (var i = 0; i < list.length; i++) {
+    var children = list[i].children
+    for (var x = 0; x < children.length; x++) {
+      children[x].innerHTML = parseInt(children[x].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild () {
+  var children = document.getElementById('grand-node')
+  var childrens = children.children[0]
+  while (childrens) {
+    children = childrens
+    childrens = children.children[0]
+  }
+  return children
+}
