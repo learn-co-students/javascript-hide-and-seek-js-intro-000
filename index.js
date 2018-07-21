e9b85c5e5781d0f2c249b0afaf975a38f14cd437
Jonathan Hosein <jonathan.hosein@live.com>
2018-07-21 21:18:41 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.getElementById('nested').querySelector('.target');
 }
+
+function increaseRankBy(n) {
+  var nums = document.querySelectorAll('.ranked-list li');
+
+  for (let i = 0; i < nums.length; i++) {
+
+    nums[i].innerHtml = parseInt(nums[i].innerHTML, 10) + n;
+  }
+}
