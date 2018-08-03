c3b32bc580aa8d80560a89e695e864467f3ff690
Alena <i.am.am.629@gmail.com>
2018-08-03 18:47:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var l = document.querySelectorAll('ul.ranked-list')
+  for (var i = 0; i < l.length; i++) {
+    l[i].innerHTML = parseInt(l[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var current = document.getElementById('grand-node')
+  var next = current.children[0]
+  while (next) {
+    current = next
+    next = current.children[0]
+  }
+  return current
+}
