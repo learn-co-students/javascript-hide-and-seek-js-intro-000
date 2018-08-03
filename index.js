e745d9b130495ee5c5579f0aab15c33606a6ddcc
joe fisher <jhanajon@gmail.com>
2018-08-03 22:04:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  const select = document.querySelector(selector); {
+ 
+  }
+  return select;
+}
+
+
+function nestedTarget() {
+  const target = document.querySelector('div.target'); {
+  }
+  return target
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list'); {
+    for (let i = 0; i < lis.length; i++) {
+      lis[i].innerHTML = (parseInt(1,10) + n).toString();
+    }
+  }
+  return lis;
+}
+
+function deepestChild() {
+
+}
 
