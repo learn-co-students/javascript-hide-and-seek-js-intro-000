d1c469d58cd5d64233491529cd7ba51ec4543f20
Brian Cortes <briancorts166@gmail.com>
2018-08-09 18:50:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var getFirstSelector = function(selector) {
+  return document.querySelector(selector)
+}
 
+var nestedTarget = function() {
+ return document.querySelector('div.target')
+}
+
+
+var increaseRankBy = function(n) {
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < list.length; i++) {
+
+    list[i].innerHTML = (parseInt(list[i].innerHTML, 10) + n).toString()
+  }
+}
+
+var deepestChild = function() {
+  return document.getElementById('grand-node').querySelector('div div div div div')
+}
