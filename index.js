ad7be3bc96e1147b3d44fd73e6cfbd57684f94a4
Chris Gabel <cwgabel@yahoo.com>
2018-08-21 19:06:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+function getFirstSelector(selector) {
+  let element = document.querySelector(selector)
+  //console.log('?', element.id == 'nested')
+  //debugger;
+  return element
+}
+
+function nestedTarget() {
+  let element = document.querySelector('#nested .target')
+  return element
+}
+
+function deepestChild() {
+  let next = '#grand-node'
+  let element = document.querySelector(next)
+  while (true) {
+    element = document.querySelector(next)
+    if (element.childElementCount === 0)
+       return element
+    else
+       next += ' ' + element.localName
+  }
+}
+
+function increaseRankBy(n) {
+  const lis = document
+  .getElementById("app")
+  .querySelectorAll("ul.ranked-list li");
+
+  for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n
+  }
+}
