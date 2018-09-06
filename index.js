8392816b3dfd110c3563b4080f375209c66ddc5f
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-06 04:43:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+      return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n){
+      var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list');
+      for (let i = 0; i < ranks.length; i++) {
+      ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+var depth = document.getElementById('grand-node').querySelectorAll('div')
+return depth[depth.length - 1]
+}
