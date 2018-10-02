0eea55d4fd01823d2124d84ec7645858648aebf7
alexanderchao123 <chao.alexander.123@gmail.com>
2018-10-02 05:03:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('.ranked-list li')
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerText = parseInt(ranks[i].innerText) + n
+  }
+}
+
+function deepestChild() {
+
+}
