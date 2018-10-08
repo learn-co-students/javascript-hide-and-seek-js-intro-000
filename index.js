f53958bd1c38da4d805295b53605fff39bd801a5
Juan Nicolas <jnma91@gmail.com>
2018-10-08 01:40:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  var currentList
+  for (var i = 0; i < rankedLists.length; i++){
+    currentList = rankedLists[i]
+    for (var j = 0; j < currentList.children.length; j++){
+      let currentNum = currentList.children[j]
+      currentNum.innerHTML = parseInt(currentNum.innerHTML) + n
+    }
+  }
+}
+
+function deepestChild(){
+  var grandNode = document.querySelector(`#grand-node`)
+  var nextUp = grandNode.querySelector('div')
+
+  while (nextUp.querySelector('div')){
+    nextUp = nextUp.querySelector('div')
+  }
+  return nextUp
+}
