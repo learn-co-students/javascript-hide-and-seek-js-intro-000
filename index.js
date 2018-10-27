a780f7599de761d8be92889c046fc0d850c3feb0
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-10-27 09:46:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('.ranked-list li');
+
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  // return document.querySelector("#grand-node div div div div"); // one line solution
+  let currentNode = document.querySelector('#grand-node');
+  let nextNode = currentNode.children[0];
+  while(nextNode) {
+    currentNode = nextNode;
+    nextNode = currentNode.children[0];
+  }
+  return currentNode;
+}
