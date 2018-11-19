5e2ef6049a2beb875cc01798e25b510c066279d4
Fei Qi <feiqi9047@gmail.com>
2018-11-19 22:50:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return getFirstSelector('#nested .target')
+}
+
+function increaseRankBy (n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(let i=0; i<lis.length;i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
+  }
+}
+
+function deepestChild(){
+  var lis = document.getElementById('grand-node').querySelectorAll('div')
+   for(let i=0; i<lis.length-1;i++){
+   var test =lis[i].querySelector('div')
+  }
+  return test
+}
