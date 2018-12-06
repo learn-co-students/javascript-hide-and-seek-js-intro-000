ae18c29c87f32e2bfa071b49a768a0ff906edb89
camille feghali <camfeg@gmail.com>
2018-12-06 10:51:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+function getFirstSelector(selector){
+  return document.querySelectorAll(selector)[0]
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").getElementsByTagName('div')[3]
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .getElementById("app")
+  .querySelectorAll("ul.ranked-list li")
+
+  for (var i = 0 ; i < lis.length ; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+
+return document.getElementById("grand-node").querySelectorAll("div")[3]
+  }
