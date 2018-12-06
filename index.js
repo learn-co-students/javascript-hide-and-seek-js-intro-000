c4cde0129a001a879923d6cb703b3070858d5fde
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-06 20:08:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget () {
+  return document.getElementById("nested").querySelector("div.target")
+}
+
+function increaseRankBy (n) {
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list");
+  for(let i = 0;i < lis.length;i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n)
+  }
+}
+
+function deepestChild () {
+  var deepcd = document.querySelectorAll("div#grand-node div");
+  return deepcd[deepcd.length-1];
+}
