313ddf1120d2432e00f12292d7098a6a4abf4767
Kevin Ngo <k.ngo93@outlook.com>
2018-12-31 00:58:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+function getFirstSelector(selector){
+  var result = document.querySelector(selector);
+
+  return result;
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var ranks = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+
+  for (var x = 0; x < ranks.length; x++) {
+    ranks[x].innerHTML = parseInt(ranks[x].innerHTML) + n;
+  }
+}
+// help
+function deepestChild() {
+  var divsNodeList = document.getElementById("grand-node").querySelectorAll("div");
+  return divsNodeList[divsNodeList.length - 1];
+}
