8fccd156b620983a31b2e933a96c9db4c25aa462
Amandine V <amavelamala@gmail.com>
2019-01-29 14:48:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested div.target');
+}
+
+function increaseRankBy(n) {
+   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+   for (let i = 0; i < lis.length; i++) {
+     var el = parseInt(lis[i].innerHTML, 10);
+     lis[i].innerHTML = (el += n).toString();
+   }
+ }
+
+function deepestChild() {
+  let parentNode = document.getElementById('grand-node');
+  let childNode = parentNode.children[0];
+
+  while (childNode != null) {
+    parentNode = childNode;
+    childNode = parentNode.children[0];
+  }
+  return parentNode;
+}
