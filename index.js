3fdb8125040280a573dc68aaeab120a2c23d2d7d
Nic Sevic <nasevic@gmail.com>
2019-03-21 13:39:34 +0000

    Done.

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var list = document.querySelectorAll('ranked-list')
+/*  var list = document.querySelectorAll('ranked-list')
   for (var i in list){
     var li = list[i].querySelectorAll('li')
     for (var i in li) {
       var toChange = li[i].textContent
       return li[i].textcontent = (parseInt(toChange, 10)+n)
     }
-  }
+  }*/
+  const rankedLists = document.getElementsByClassName('ranked-list');
+   for (let i = 0; i < rankedLists.length; i++) {
+     rankedLists[i].textContent = (parseInt(rankedLists[i].textContent) + n)
+   }
 }
 
 function deepestChild() {
   var nested = document.getElementById('grand-node')
-  while (nested.lastElementChild > 0){
+  var egg = nested.children[0]
+  while (egg){
 
-    nested = nested.querySelectorAll('div')
+    nested = egg
+    egg = nested.children[0]
   }
   return nested
   /*while (nested.length) {
