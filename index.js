465af9c1cebbb1be7250f511f4b5b15438ce3bb0
Nic Sevic <nasevic@gmail.com>
2019-03-21 13:28:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll('ranked-list')
+  for (var i in list){
+    var li = list[i].querySelectorAll('li')
+    for (var i in li) {
+      var toChange = li[i].textContent
+      return li[i].textcontent = (parseInt(toChange, 10)+n)
+    }
+  }
+}
+
+function deepestChild() {
+  var nested = document.getElementById('grand-node')
+  while (nested.lastElementChild > 0){
+
+    nested = nested.querySelectorAll('div')
+  }
+  return nested
+  /*while (nested.length) {
+    var fullList = nested
+    fullList.concat(querySelectorAll('div'))
+    return fullList
+  }*/
+}
