1d2bc74390004453247e34e66a883a616ca5a05a
Maxi Learner <sagmeister@gmail.com>
2019-04-15 21:38:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list')
+    for (let i = 0; i < rankedList.length ; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
