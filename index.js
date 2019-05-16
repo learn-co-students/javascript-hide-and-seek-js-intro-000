bd5cc457351345a383fdbbbe11e33cac6bc7f843
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-16 04:28:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHtml))+n)
+  }
+}
+function deepestChild(){
+  var deepChild = document.getElementById('grand-node').querySelectorAll('div')
+  return deepChild[deepChild.length-1]
+}
