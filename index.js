26dd2700ed8d8dfbc2267b2c31c88d9a3aab6725
Brad Ayers <brad@bradayers.com>
2019-05-18 04:13:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  //return document.querySelector(#nested .target);
+  //find #nested
+  //then find .target
+  console.log(document.querySelector('#nested .target'));
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n;
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0]  ;
+
+  while (nextNode) { 
+    node = nextNode ;
+    nextNode = node.children[0] ;
+  }  
+
+  return node ;
+}
