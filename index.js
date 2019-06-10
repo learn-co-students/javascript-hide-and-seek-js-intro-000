45449d537f3350313919e5002914027416b91fe6
Stephanie Grande <stephanie.grande34@gmail.com>
2019-06-10 14:49:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < rankedList.length; i++) {
+    let children = rankedList[i].children;
+
+    for (let v = 0; v < children.length; v++) {
+      children[v].innerHTML = parseInt(children[v].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let parent = document.getElementById('grand-node');
+  let child = parent.children[0];
+
+  while (child) {
+    parent = child;
+    child = parent.children[0];
+  }
+  return parent;
+}
