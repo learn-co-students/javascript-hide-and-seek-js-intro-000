a0a66000171d3127e451dbf9ac89e61ee2e402a1
Asa Tirsen <asa.tirsen@gmail.com>
2019-06-20 02:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function getFirstSelector(selector) {
+   return document.querySelector(selector)[0];
+}
+
+function nestedTarget() {
+  const elements = document.querySelector('#nested.target');
+  return elements;
+}
