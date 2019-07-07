6d873726fec24eb2f88f072585efae7644d9be1e
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 10:03:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+var gatFirstSelector = (selector) => {
+  return document.querySelector(selector)
+}
 
+var nestedTarget = () => {
+  document.getElementById('nested').querySelector('target')
+
+  return document.querySelector('#nested .target')
+}
+
+var increaseRankBy = (n) => {
+  var elements = document.querySelectorAll('.rankedl-list')
+
+  for(let i = 0; i < elements.length; i++) {
+    elements[i] = parseInt(elements[i], 10) + n
+  }
+
+  return
+}
+
+var deepestChild = () => {
+  var mainNode = document.querySelectorAll('div').getElementById('grand-node')
+  var nextNode = mainNode.children[0]
+
+  while(nextNode) {
+    mainNode = nextNode
+    nextNode = mainNode.children[0]
+  }
+
+  return mainNode[0]
+}
