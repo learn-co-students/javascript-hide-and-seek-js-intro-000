189b4a501d70b1a7074e1b0de873979e734cf30b
Hema Venkata <hvenkata@live.com>
2019-07-08 21:55:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+   return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var newRanks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < newRanks.length; i++) {
+    newRanks[i].innerHTML = parseInt(newRanks[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var deepest = document.getElementById('grand-node').querySelectorAll('div')
+  return deepest[deepest.length-1]
+}
