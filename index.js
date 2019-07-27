1790956af960f1b6f0b10460a4a7122516c70bc9
Christos Giakoumis <cm.giakoumis@gmail.com>
2019-07-27 16:30:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector)
+  {
+    var test1 = document.querySelector(selector);
+    return document.querySelector(selector);
+  }
 
+function nestedTarget()
+  {
+    return document.querySelector('#nested .target');
+  }
+
+function increaseRankBy(n)
+  {
+    var list = document.querySelectorAll('ul.ranked-list li')
+      for (var i = 0; i < list.length; i++)
+        {
+          list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+        }
+  }
+
+  function deepestChild() {
+    var theNode = document.querySelector('#grand-node')
+    var nextNode = theNode.children[0];
+
+    while (nextNode) {
+      theNode = nextNode;
+      nextNode = theNode.children[0];
+    }
+    return (theNode);
+  }
