88d58d4137198ae007b10609382fdab90b06c66c
JaimieLee Barnes <barnes.jaimielee@outlook.com>
2019-08-11 03:07:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild() {
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+}
+
+function increaseRankBy(n) {
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+
+  for (let i = 0; i < upRank.length; i++) {
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+  }
+}
