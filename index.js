13ae22f22dc297b93bb362306377d9806d975db8
Matthew Danielson <danielsonmatthew4@gmail.com>
2019-08-12 18:48:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n){
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for(let i = 0; i < rank.length; i++){
+    rank[i].innerHTML = ((parseInt(rank[i].innerHTML)))
+  }
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length - 1]
+}
