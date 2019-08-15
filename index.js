b2c0304581c88a27693eb0e96dc6b36a70a75c6a
Hamza Qureshi <hamza.qureshi97@gmail.com>
2019-08-15 15:25:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+function getFirstSelector ( selector ) {
+  return document.querySelector(selector)
+}
+
+
+function nestedTarget (){
+
+  var container = document.querySelector("#nested");
+  var matches = container.querySelector(".target");
+  return matches
+}
+
+
+function increaseRankBy(n) {
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML =  parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+
+const t1 = document.querySelector('#grand-node div div div div')
+return t1
+}
