e93df206b1eb32e440a1044b0e8feb81f9875696
Tabitha Tallent <missinlucky004@aol.com>
2019-09-02 18:16:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+function deepestChild(){
+  var node=document.getElementById('grand-node')
+  var nextNode=node.children[0]
 
+  while(nextNode){
+    node=nextNode;
+    nextNode=node.children[0]
+  }
+  return node
+}
+function increaseRankBy(n){
+  var rankedLists = document.querySelectorAll('.ranked-list');
+
+ for (let i = 0, l = rankedLists.length; i < l; i++) {
+   let children = rankedLists[i].children;
+
+   for (let j = 0, k = children.length; j < k; j++) {
+     children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+   }
+ }
+}
