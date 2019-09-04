267aec08aed776df86e6a551a3b9edb3f0f74c95
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-04 22:13:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+
+function nestedTarget() {
+  var nested = document.getElementById('nested')
+  return nested.querySelector('.target')
+}
+
+function increaseRankBy (n) {
+  var rankList = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < rankList.length; i++) {
+    rankList[i].innerHTML = (parseInt(rankList[i].innerHTML) + n)
+
+  }
+}
+
+/*
+var rankedList = document.querySelectorAll('.ranked-list')
+console.log(document.querySelectorAll('.ranked-list'))
+console.log(document.querySelectorAll('ul.ranked-list li'))
+*/
+
+function deepestChild () {
+  var divList = document.querySelectorAll('#grand-node')[0].querySelectorAll('div')
+  return divList[3]
+}
+
+console.log(document.querySelectorAll('#grand-node')[0].querySelectorAll('div'))
