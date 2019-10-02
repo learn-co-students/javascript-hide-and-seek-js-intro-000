7b74967d7852b961901e015918438c55694f2c99
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-10-02 06:20:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-function getFirstSelector(selector){
+function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
 function nestedTarget(){
-  document.querySelector('#nested' .target)
+  return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  let nodes = document.querySelectorAll('#grand-node div');
+  return nodes[nodes.length - 1];
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('.ranked-list');
+  for(let i = 0; i < ranks.length; i++) {
+    let rank = ranks[i];
+    console.log(`${rank.innerHTML} ------->`)
+
+    let children = rank.children;
+    for(let j = 0; j < children.length; j++) {
+      let child = children[j];
+
+      console.log(`${child.textContent} ------->`)
+      child.innerHTML = parseInt(child.textContent) + n;
+    }
+
+  }
+}
+
+/* function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}*/
