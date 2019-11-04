8df637fec0c6ba604805504fd60c48b5f5c2909d
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-04 17:54:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  const first = document.querySelector(selector);
+  return first;
+}
 
+function nestedTarget() {
+  const nested = document.querySelector('#nested div.target');
+  return nested;
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  const deep = document.querySelector('div#grand-node div div div div');
+  return deep;
+}
