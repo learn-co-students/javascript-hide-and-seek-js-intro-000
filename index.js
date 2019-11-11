da023e9154d16376c406d2254f91ed2c6fa97191
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-11 20:48:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  let ranks = document.querySelectorAll('.ranked-list li')
+  for (let i=0; i<ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var nodes = document.getElementById('grand-node').querySelectorAll(`div`);
+  return nodes[nodes.length-1];
+}
