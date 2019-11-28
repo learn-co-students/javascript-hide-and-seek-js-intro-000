3dda52a5b4c975e85221327f66090875883c8c39
Sasa Trkulja <kamaradsasa@gmail.com>
2019-11-28 12:35:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+function increaseRankBy(n) {
+  var listItems = document.querySelectorAll('.ranked-list li');
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].innerHTML = (parseInt(listItems[i].innerHTML) + n).toString();
+  }
+}
