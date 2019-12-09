7dd8dd0a2663a6c9a0b842989dbb065c0a7318e5
github username <github email address>
2019-12-09 18:10:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector)
+}
 
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+    return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(num) {
+    const parsed  = parseInt(num)
+    const lists = document.querySelectorAll('.ranked-list')
+    console.log(lists)
+    let first = lists[0]
+    let firstLis = first.querySelectorAll('li')
+    let second = lists[1]
+    let secondLis = second.querySelectorAll('li')
+    // debugger;
+    for(let i = 0; i < firstLis.length; i++) {
+        let rank = parseInt(firstLis[i].innerHTML)
+        let newRank = parsed + rank
+        firstLis[i].innerHTML = `${newRank}`
+    }
+    for(let i = 0; i < secondLis.length; i++) {
+        let rank = parseInt(secondLis[i].innerHTML)
+        let newRank = parsed + rank
+        secondLis[i].innerHTML = `${newRank}`
+    }
+}
+
+// increaseRankBy(3)
