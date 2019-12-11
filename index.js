6bcaed0991f2e78b905970e0cf6dd32c73728694
Gerald W Stelly <gstelly319@gmail.com>
2019-12-11 04:07:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function deepestChild() {
+  var lis = document.getElementById("grand-node").querySelectorAll("div");
+  return lis[lis.length - 1];
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll("ul.ranked-list li");
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
