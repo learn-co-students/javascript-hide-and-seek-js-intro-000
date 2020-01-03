e24e2b05085bc04d93a819a2e742798e19d6eeb1
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-03 05:31:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// Define a function getFirstSelector(selector),
+// which accepts a selector and returns the first element that matches
 
+function getFirstSelector(selector) {
+
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+
+function increaseRankBy(n) {
+  const elms = document.querySelectorAll('.ranked-list li');
+  for (let i=0; i<elms.length; i++) {
+    elms[i].innerHTML = parseInt(elms[i].innerHTML)+n;
+  }
+}
