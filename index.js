f03510711762406a760ef5ef40cccad2470e658f
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-01-29 18:57:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  var incRank = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < incRank.length; i++) {
+    incRank[i].innerHTML = parseInt(incRank[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var deepest = document.getElementById('grand-node').querySelectorAll('div')
+  return deepest[deepest.length-1]
+}
