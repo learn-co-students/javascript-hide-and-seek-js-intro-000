3bcb134b2777f643647c5f42dc655e05bba61d8e
Ravi <kkiran.ror@gmail.com>
2020-03-15 02:21:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 
+function getFirstSelector(selector) {
+  const firstElement = document.querySelector(selector);
+  return firstElement;
+}
+
+function  nestedTarget(){
+  const firstElement = document.querySelector("#nested .target");
+  return firstElement;
+}
+
+function  deepestChild(){
+  const firstElement = document.querySelector('#grand-node div div div div');
+  return firstElement;
+
+}
+
+function  increaseRankBy(n){
+  let val = 0
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
+
+  let children = firstList.children
+
+  for (let i = 0, l = children.length; i < l; i++) {
+    val = parseInt(children[i].innerHTML) + n;
+     children[i].innerHTML = val;
+  }
+
+  let children2 = secondList.children
+
+  for (let i = 0, l = children2.length; i < l; i++) {
+    val = parseInt(children2[i].innerHTML) + n;
+     children2[i].innerHTML = val;
+  }
+
+}
