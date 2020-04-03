a81d079ca31b70e29cfad2aa70ee7ecfff0903d0
Sydney Michael <m13s19madden@gmail.com>
2020-04-03 23:53:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(sel) {
+  return document.querySelector(sel)
+}
 
+function nestedTarget() {
+  return document.querySelector('div#nested div.target')
+}
+
+function increaseRankBy(n) {
+  const all = document.querySelectorAll('main#app ul.ranked-list li')
+  for (let i = 0; i < all.length; i++) {
+    all[i].innerHTML = (parseInt(all[i].innerHTML) + n).toString();
+  }
+  return all
+}
+
+function deepestChild() {
+    return document.querySelector('div#grand-node div div div div')
+}
