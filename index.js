15e1b67455341a6fe51539a16c781a453542a4f4
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-05 20:59:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector){
+ return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild(){
+  document.querySelectorAll('div.#grand-node')
+  
+  for (let i = 0; i < document.length; i++) {
+    if (document.querySelector(child)){
+      var newEle = child
+    }
+  }
+  return newEle
+}
+	
 
