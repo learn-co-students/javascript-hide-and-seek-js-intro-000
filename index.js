e324b62052fa95f62af673a9dd27237a54d6b876
github username <github email address>
2020-04-14 15:44:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function deepestChild() {
+  const div = document.querySelectorAll('#grand-node div')
+  return div[div.length - 1]
+}
+
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+  }
+
+  
