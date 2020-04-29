10e72524390879a2b6cf0831013cb2cb28efe3e2
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-29 19:36:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
 
+function nestedTarget()
+{
+  return document.querySelector(`#nested .target`)
+}
+
+function deepestChild()
+{
+  var current = document.querySelector(`#grand-node`)
+  while(current.children[0] != undefined)
+  {
+    current = current.children[0]
+  }
+  return current
+}
+
+function increaseRankBy(n)
+{
+  var arr = document.querySelectorAll(`ul.ranked-list li`);
+  for (let i = 0; i < arr.length; i += 1)
+  {
+    arr[i].innerHTML = ((parseInt(arr[i].innerHTML) + n).toString());
+  }
+}
