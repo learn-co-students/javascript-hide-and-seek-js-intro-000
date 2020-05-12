b0e3da3f3b9c60b10ef714ebb145693fad35fe73
Allen <developer2019@icloud.com>
2020-05-12 03:31:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let deepest = document.getElementById('grand-node').querySelectorAll('div')
+  return deepest[deepest.length-1];
+}
