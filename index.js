aaf97ead7d23936073879e4bc67e10d4155e9668
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-07 06:41:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const listItems = document.querySelectorAll('.ranked-list li')
+
+  for (let i in listItems){
+    const parseString = parseInt(listItems[i].innerHTML)
+    listItems[i].innerHTML = `${parseString + n}`
+  }
+}
+
+function deepestChild(){
+  let current = document.querySelector('div#grand-node')
+  let next;
+
+  while (current){
+
+    if (current.querySelector('div') != undefined){
+      next = current
+      current = next.querySelector('div')
+    }
+    else {
+      return current
+    }
+  }
+}
