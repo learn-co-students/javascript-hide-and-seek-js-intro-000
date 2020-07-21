d6172fa1451dbf6db6a8cd18ebc77ca81bf40ab2
Eric C <erix2205@gmail.com>
2020-07-21 14:53:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  var result = document.querySelector(selector);
+  return result
+}
 
+function nestedTarget(){
+  var result = document.getElementById('nested').querySelector('.target')
+  return result
+}
+
+function increaseRankBy(n){
+  const main = document.querySelectorAll('.ranked-list li');
+  for (let i=0; i< main.length ; i++){
+    var x = parseInt(main[i].innerHTML, 10)
+    x = x + n
+    main[i].innerHTML = x
+  }
+}
+
+function deepestChild(){
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while(nextNode){
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node
+}
