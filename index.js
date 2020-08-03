7b392feea3fc50ca9bac8e1ff1212b4a83b9892e
Akshay Niranjan <anstmfriend@gmail.com>
2020-08-03 23:04:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function deepestChild(){
+  var start=document.getElementById("grand-node");
+  while(start.querySelector("div")!=null){
+    start=start.querySelector("div");
+  }
+  return start;
+}
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+function nestedTarget(){
+  return document.querySelector(".target");
+}
