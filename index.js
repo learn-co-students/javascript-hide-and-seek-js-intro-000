f2ac279c3164625d69cc2e638e48a1ae8e8872ae
Garrett Breach <breachg1992@gmail.com>
2020-12-10 21:01:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  let target = document.querySelector('div#nested div div div div')
+  return target
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('ul.ranked-list li');
+
+  for (let i =0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  let hellaDeep = document.getElementById('grand-node').querySelectorAll('div');
+  return hellaDeep[hellaDeep.length - 1]
+}
