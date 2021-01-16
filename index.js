195f00014c6c555b536b375fb80c5c2086659056
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-16 18:07:57 -0500

    Done.

--- index.js
+++ index.js
@@ -4,22 +4,37 @@ let getFirstSelector = (sel) => {
 };
 
 
+
+
+
+
+
 let nestedTarget = () => {
     return document.getElementsByTagName('div')[4];
 };
+
+
+
 
 
 let deepestChild = () => {
 return document.querySelector("#grand-node div div div div")
 };
 
+
+
+
+
+
 let increaseRankBy = (n) => {
+
 let rankedList = document
     .getElementById("app")
     .querySelectorAll("ul.ranked-list li");
-   
-for (let i = 0; i < rankedList; i++) {
-    parseInt(rankedList[i]).innerHTML = (i + n).toString();
-    return rankedList
-}
+
+    for (let i = 0; i < rankedList.length; i++) {
+        let textContent = Number(rankedList[i].textContent)
+    rankedList[i].innerHTML = (textContent + n).toString();
+    
+    }
 };
