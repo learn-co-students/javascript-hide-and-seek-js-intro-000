abff709e29753824e47a15203d03655650875fa3
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-16 13:51:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+let getFirstSelector = (sel) => {
+   return document.querySelector(`${sel}`);
+};
+
+
+let nestedTarget = () => {
+    return document.getElementsByTagName('div')[4];
+};
+
+
+let deepestChild = () => {
+
+};
+
+let increaseRankBy = (n) => {
+
+};
