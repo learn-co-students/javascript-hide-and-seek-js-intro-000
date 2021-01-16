f6af3c12b47b6ed2c1082b4569018fb4322fb78b
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-16 17:19:11 -0500

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ let nestedTarget = () => {
 
 
 let deepestChild = () => {
-
+return document.querySelector("#grand-node div div div div")
 };
 
 let increaseRankBy = (n) => {
-
+let rankedList = document
+    .getElementById("app")
+    .querySelectorAll("ul.ranked-list li");
+   
+for (let i = 0; i < rankedList; i++) {
+    parseInt(rankedList[i]).innerHTML = (i + n).toString();
+    return rankedList
+}
 };
