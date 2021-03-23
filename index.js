619364e875ec5b6b11d190aef2a4de46e8033888
Spartak Belov <job.spartak@gmail.com>
2021-03-23 23:24:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(s){
+  return document.querySelector(s);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+ function increaseRankBy(n){
+   const list = document.querySelectorAll('.ranked-list');
+   for(let i = 0; i < list.length; i++){
+     for(let k = 0; k < list[i].children.length; k++){
+       list[i].children[k].innerHTML = parseInt(list[i].children[k].innerHTML)+n;
+     }
+   }
+ }
+
+function deepestChild(){
+  var list = document.querySelector('#grand-node');
+  while(true){
+    if(list.children[0]){
+      list = list.children[0];
+    }else{
+      return list;
+    }
+  }
+}
