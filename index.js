c8733b1d36cdc7359259006e6e7ad5ba173ceb56
ji sun lee <ellijs@hotmail.com>
2021-03-25 20:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  let a = document.querySelector(selector)
+  return a;
+}
 
+function nestedTarget() {
+  let b = document.querySelector("div.target");
+  return b;
+}
+
+function increaseRankBy(n) {
+  let a = document
+    .getElementByID("app")
+    .querySelectorAll("ul.ranked-list li");
+    
+  for (let i = 0; i < a.length; i++) {
+    a[i].innerHTML = parseInt(a[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let a = document
+    .getElementByID("grand-node")
+    .querySelector("div.grand-node div div div div");
+  return a;
+}  
