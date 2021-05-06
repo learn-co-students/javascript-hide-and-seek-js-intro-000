82a27807de0981f0f6bf892280948a47973a1ca6
Diego Domínguez Melo <diego_14melo@hotmail.com>
2021-05-06 23:39:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const getFirstSelector = selector => document.querySelector(selector);
+
+const nestedTarget = () => document.querySelector("#nested .target");
+
+const increaseRankBy = n => {
+  let lists = document.querySelectorAll("ul.ranked-list li");
+  for (var i = 0; i < lists.length; i++) {
+    lists[i].innerHTML = n + parseInt(lists[i].innerHTML)
+  }
+}
+
+const deepestChild = () => {
+  let root = document.getElementById("grand-node");
+  let nodes = root.querySelectorAll("div");
+  let deepest = nodes[nodes.length - 1]
+  return deepest;
+}
