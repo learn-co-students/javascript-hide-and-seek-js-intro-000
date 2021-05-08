97d3e8584446c514cad9925386ae196b82db2efb
Robert Killion <robertfkillion@gmail.com>
2021-05-08 12:09:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+let getFirstSelector = (selector) => document.querySelector(selector);
+let nestedTarget = () => document.querySelector("#nested .target");
+function increaseRankBy(n) {
+  let listItems = document.querySelectorAll(".ranked-list li");
+  var num = 0;
+  for (let i=0;i<listItems.length;i++) {
+    num = parseInt(listItems[i].innerHTML);
+    num = num+n;
+    listItems[i].innerHTML = num.toString();
+  }
+}
+let deepestChild = () => document.querySelector("#grand-node div div div div");
