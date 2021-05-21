7351fe6c32129e05f56ed8f968c1b37a33193a38
Daniel Calvin <arockne2003@yahoo.com>
2021-05-21 21:19:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+  if (typeof selector !== 'string') {
+    return null;
+  }
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('ul.ranked-list');
+  for (let list of rankedList) {
+    for (let listItem of list.querySelectorAll('li')) {
+      listItem.innerHTML = parseInt(listItem.innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  const grandNode = document.querySelector('#grand-node');
+  for (let next = grandNode; next !== null; next = next.querySelector('div')) {
+    if (next.querySelector('div') === null) {
+      return next;
+    }
+  }
+}
